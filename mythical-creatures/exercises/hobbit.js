0c0b39837cfcfc2105232a4a0418c007c8f6a5fa
Geena Jackson <gjacks303@gmail.com>
2021-12-10 08:14:16 -0700

    Add hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,27 +3,31 @@ class Hobbit {
     this.name = hobbit.name;
     this.age = 0;
     this.adult = false;
+    this.old = false;
+    this.hasRing = true;
     }
 
   celebrateBirthday() {
     this.age += 1;
-    if (this.age <= 32)
-    return true;
-    if (this.age > 32)
-    return false;
+    if (this.age <= 32) {
+      return this.adult = false
+    }
+    if (this.age > 100) {
+      return this.old = true
+    }
+    return this.adult = true;
   }
 
-  // timeTravel() {
-  //   (32 <= this.age <= 101)
-  //
-  //   if (this.age <= 32) {
-  //     return true
-  //   } else (this.age > 32) {
-  //     return false
-  //   } else if (this.age )
-  // }
+  getRing() {
+    if (this.name === "Frodo") {
+    this.hasRing === true
+      return 'Here is the ring!';
+  } else {
+    this.hasRing = false
+    return 'You can\'t have it!';
+    }
+  }
 }
-// 0-32 = child
-// 33 - 100 = adult
-// 100+ = old
+
+
 module.exports = Hobbit;
