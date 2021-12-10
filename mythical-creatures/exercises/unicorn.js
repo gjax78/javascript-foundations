11509e39f0d84f86401c1a42493b0ab4747d0d83
Geena Jackson <gjacks303@gmail.com>
2021-12-10 08:14:50 -0700

    Add unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,15 +1,34 @@
-class Unicorn {
-  constructor(name,color) {
-  this.name = name;
-  this.color = color || "white";
-  }
 
-  isWhite() {
-    return false
-  }
-  says(message) {
-    return `**;* ${message} *;**`
-  }
-}
 
-module.exports = Unicorn;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Unicorn {
+//   constructor(name,color) {
+//   this.name = name;
+//   this.color = color || "white";
+//   }
+//
+//   isWhite() {
+//     return false
+//   }
+//   says(message) {
+//     return `**;* ${message} *;**`
+//   }
+// }
