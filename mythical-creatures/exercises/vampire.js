485353c8f84fea7a0c3627a42f705b1a45cf55fb
Geena Jackson <gjacks303@gmail.com>
2021-12-17 11:11:43 -0700

    Add vampire

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -6,12 +6,13 @@ class Vampire {
     this.ouncesDrank = 0;
   }
   drink() {
-    this.thirsty = false;
-    this.ouncesDrank += 10;
-    if (this.ouncesDrank > 50) {
-      return 'I\'m too full to drink anymore!';
+      this.thirsty = false;
+      if (this.ouncesDrank < 50) {
+        this.ouncesDrank += 10
+      }
+      return "I\'m too full to drink anymore!"
     }
   }
-}
+
 
 module.exports = Vampire
