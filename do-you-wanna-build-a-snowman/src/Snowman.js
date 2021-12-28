f9c51c4d2c0e654ca802f3c8cedc4b8a7001d37d
Geena Jackson <gjacks303@gmail.com>
2021-12-27 19:50:58 -0700

    Add snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,29 @@
 class Snowman {
+  constructor(snowman) {
+    this.carrots = snowman.carrots
+    this.coal = snowman.coal
+    this.buttons = snowman.buttons
+    this.snowballs = snowman.snowballs
+    this.magicHat = false
+  }
 
+  canWearMagicHat() {
+    if (this.coal >= 2) {
+      this.magicHat = true
+    }
+    if (this.coal < 2) {
+      this.magicHat = false
+    }
+    if (this.buttons < 5) {
+      this.magicHat = false
+    }
+    if (this.carrots < 1) {
+      this.magicHat = false
+    }
+    if (this.snowballs < 2) {
+      this.magicHat = false
+    }
+  }
 }
 
 module.exports = Snowman;
