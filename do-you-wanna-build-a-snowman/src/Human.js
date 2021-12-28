ad237e203daa35ad444472f5c0be4aed7e37029c
Geena Jackson <gjacks303@gmail.com>
2021-12-27 19:50:44 -0700

    Add human

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,32 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(human) {
+    this.name = "Kayla"
+    this.wantsToBuildASnowman = true
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+  }
 
+  gatherMaterials(material, amount) {
+    this.materials[material] += amount;
+  }
+
+  buildASnowman() {
+    this.snowman = new Snowman(this.materials)
+    return this.snowman
+  }
+
+  placeMagicHat(snowman) {
+    if (this.snowman.magicHat === true) {
+      return 'Woah, this snowman is coming to life!'
+    }
+    return 'I guess I didn\'t build it correctly.'
+  }
 }
 
 module.exports = Human;
