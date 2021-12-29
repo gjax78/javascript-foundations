cbdfa03136663de28d25f9deba29ef4f31223963
Geena Jackson <gjacks303@gmail.com>
2021-12-29 14:42:29 -0700

    Add lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner
+    this.material = lunchbox.madeOf
+    this.shape = lunchbox.shape
+    this.color = lunchbox.color
+    this.snacks = []
+  }
 
+  acquireSnack(snackType) {
+    this.snacks.push(snackType)
+    snackType.isInLunchBox = true
+  }
+
+  findHealthySnacks(healthySnacks) {
+    healthySnacks = []
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type)
+      }
+    }
+    return healthySnacks
+  }
 }
 
 module.exports = LunchBox;
