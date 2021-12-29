9fbb9b9081a8bef26dec3e712251dc1ce045ced8
Geena Jackson <gjacks303@gmail.com>
2021-12-29 14:42:57 -0700

    Add snack

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,25 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra'
+    this.type = type
+    this.amount = 100
+    this.isInLunchBox = false
+    this.cuttingItClose = false
+  }
 
+  getEaten() {
+    this.amount -= 10
+    if ((this.amount / 100) <= 0.2) {
+      this.cuttingItClose = true
+    }
+  }
+
+  checkForHealthy() {
+    if (this.type.toLowerCase().includes('fruit')) {
+      return true
+    }
+    return false
+  }
 }
 
 module.exports = Snack;
