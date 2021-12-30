2c94c0566ae3d7b21a62e69a46ddd46e13ca6d36
Geena Jackson <gjacks303@gmail.com>
2021-12-30 13:07:12 -0700

    Add medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor (name) {
+    this.name = name
+    this.statues = []
+  }
+
+  gazeAtVictim(person) {
+    var person = new Statue(person.name)
+    this.statues.push(person)
+    if (this.statues.length > 3) {
+      var freedPerson = new Person(this.statues[0].name)
+      freedPerson.mood = 'relieved'
+      this.statues.shift()
+      return freedPerson
+    }
+  }
+}
+
+
+
+
+module.exports = Medusa;
